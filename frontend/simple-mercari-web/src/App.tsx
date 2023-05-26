d61f5a513ca90dd12c51d958dad35ece11c36860
nachan28 <torajiro282828@gmail.com>
2023-05-26 14:31:43 +0900

    create and set context provider

--- frontend/simple-mercari-web/src/App.tsx
+++ frontend/simple-mercari-web/src/App.tsx
@@ -6,25 +6,28 @@ import { UserProfile } from "./components/UserProfile";
 import { Listing } from "./components/Listing";
 import "./App.css";
 import { Header } from "./components/Header/Header";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ItemsProvider } from "./common/context";
 
 export const App: React.VFC = () => {
   return (
     <>
-      <ToastContainer position="bottom-center"/>
+      <ItemsProvider>
+        <ToastContainer position="bottom-center" />
 
-      <BrowserRouter>
-        <div className="MerComponent">
-          <Header></Header>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/item/:id" element={<ItemDetail />} />
-            <Route path="/user/:id" element={<UserProfile />} />
-            <Route path="/sell" element={<Listing />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+        <BrowserRouter>
+          <div className="MerComponent">
+            <Header></Header>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/item/:id" element={<ItemDetail />} />
+              <Route path="/user/:id" element={<UserProfile />} />
+              <Route path="/sell" element={<Listing />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ItemsProvider>
     </>
   );
 };
